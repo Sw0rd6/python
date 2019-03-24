6e822a592ac55848e2a179a457997e45ff1102c0
wuhan <43577526+honorwh@users.noreply.github.com>
2019-03-24 11:03:04 +0800

    Create select_sort.py

---
+++ python日常/排序算法/select_sort.py
@@ -0,0 +1,13 @@
+#选择排序法：每次选出序列中的最小或者最大元素，存放到排序序列的相应位置，直到未排序序列长度为０
+#要注意的点就是在选定最值的时候，必须先确定min或者max的下标之后，再进行交换，才能确保是最值。
+#假设给定序列为ls
+def select_sort():
+	n = len(ls)
+	for i in range(n):
+		min = i
+		for j in range(i + 1, n):
+			if ls[j] < ls[min]:
+				min = j
+			ls[j], ls[min] = ls[min], ls[j]
+	return ls
+				
