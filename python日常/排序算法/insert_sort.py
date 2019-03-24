9140575a76b4d8422e5144b9761700b2238718ef
wuhan <43577526+honorwh@users.noreply.github.com>
2019-03-24 11:38:56 +0800

    Create insert_sort.py

---
+++ python日常/排序算法/insert_sort.py
@@ -0,0 +1,16 @@
+#插入排序：构建有序序列，序列第一个元素被视为已排序，后续的就通过对已排序的序列进行从后到前的扫描，在相应的位置插入即可
+#设给定的序列为ls
+def insert_sort():
+	n = len(ls)
+	for i in range(1, n):
+		if ls[i - 1] > ls[i]:
+			temp = ls[i]
+			index = i
+			for j in range(i - 1, -1 , -1):		#插入排序的重点就在于这个循环的参数，所谓的从后往前扫描 -> (i - 1, -1, -1)
+				if ls[j] > temp:
+					ls[j + 1], ls[j] = ls[j], ls[j + 1]
+					index = j
+				else:
+					break
+			#ls[index] = temp
+	return ls
